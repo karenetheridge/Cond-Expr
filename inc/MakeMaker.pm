1ecf7fabcb20a80737db9a218e9fa7e418597ea0
Karen Etheridge <ether@cpan.org>
2015-10-06 19:35:17 -0700

    under metaspec v2.0, we do not dynamically determine prereqs so dynamic_config = 0

--- inc/MakeMaker.pm
+++ inc/MakeMaker.pm
@@ -5,7 +5,6 @@ use lib 'inc';
 use MMHelper;
 
 extends 'Dist::Zilla::Plugin::MakeMaker::Awesome';
-with 'Dist::Zilla::Role::MetaProvider';
 
 override _build_MakeFile_PL_template => sub {
     my ($self) = @_;
@@ -20,10 +19,6 @@ override _build_MakeFile_PL_template => sub {
 
     return $tmpl;
 };
-
-sub metadata {
-    return { dynamic_config => 1 };
-}
 
 override _build_WriteMakefile_args => sub {
     my ($self) = @_;
